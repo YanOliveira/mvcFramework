c6235a630fd0580ea9c3bd1ca3d29c4e6ca0e622
Yan Oliveira <oliveira.yan02@gmail.com>
2019-05-13 19:32:46 -0300

    routes

--- mvc/core/App.php
+++ mvc/core/App.php
@@ -2,7 +2,10 @@
 class App {
   public function run(){
     $url = isset($_GET['url']) ? "/".$_GET['url'] : "/";               
-    $url = explode("/", $url);      
+    $url = $this->checkRoute($url);    
+    echo $url;
+    $url = explode("/", $url);
+    
   
     $currentController = (!empty($url[1]) ? ucfirst($url[1]) : ucfirst(CONFIG_DEFAULT_CONTROLLER))."Controller";
     $currentAction = !empty($url[2]) ? $url[2] : CONFIG_DEFAULT_ACTION;      
@@ -18,5 +21,30 @@ class App {
       $error = new NotFoundController();
       $error->index(404);
     }
+  }
+
+  private function checkRoute($url){
+    global $routes;
+    foreach($routes as $key => $newurl){
+      $pattern = preg_replace('(\{[a-z0-9]{1,}\})', '([a-z0-9-]{1,})', $key);            
+      if(preg_match('#^('.$pattern.')*$#i', $url, $matches) === 1){
+        array_shift($matches);
+        array_shift($matches);
+        $itens = array();
+        if(preg_match_all('(\{[a-z0-9]{1,}\})', $key, $m)){
+          $itens = preg_replace('(\{|\})', '', $m[0]);                    
+        }
+        $args = array();
+        foreach($matches as $key => $match){
+          $args[$itens[$key]] = $match;          
+        }      
+        foreach($args as $key => $value){
+          $newurl = str_replace(':'.$key, $value, $newurl);          
+        }   
+        $url = $newurl;             
+        break;
+      }
+    }
+    return $url;
   }
 }
--- mvc/index.php
+++ mvc/index.php
@@ -2,6 +2,7 @@
 session_start();
 require "config.php";
 require "autoload.php";
+require 'routes.php';
 
 $app = new App;
 $app->run();
