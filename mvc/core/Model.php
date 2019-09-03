c45194651133709a807408feb6c6bb2746b58424
Yan Oliveira <oliveira.yan02@gmail.com>
2019-09-03 10:40:46 -0300

    documentaçao Classe Model

--- mvc/core/Model.php
+++ mvc/core/Model.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Classe Model
+ * Padroniza o comportamento e alguns métodos para todos os models. 
+ * @package core
+ * @version 1.0.0
+ * @author Yan Oliveira <oliveira.yan02@gmail.com> 
+ **/
 abstract class Model{  
   
   public function __construct(){    
@@ -6,7 +13,17 @@ abstract class Model{
     $pdo = Connection::getInstance();    
   }      
 
-  public function executeQuery($query, $params = null){
+  /**
+   * Executa uma query no db utilizando o prepare statements do PDO.
+   * Recebe uma string com query a ser preparada e um array com os parâmetros.   
+   * Ex.: executeQuery('select * from tabela where id=?', array(1));
+   * @access protected
+   * @param String $query
+   * @param Array $params
+   * @return Array
+   * @example executeQuery('select * from tabela where id=?', array(1))
+  **/ 
+  protected function executeQuery($query, $params = null){
     global $pdo;
     try {                                       
         $sql = $pdo->prepare($query);                 
@@ -24,21 +41,5 @@ abstract class Model{
     } catch (PDOException $e) {
         echo "Erro: " . $e->getMessage();
     }
-  }  
-
-  // public function executeQueryMultipleResult($query, $params){
-  //   global $pdo;
-  //   try {
-  //       $sql = $pdo->prepare($query);
-  //       $sql->execute($params);
-  //       if ($sql->rowCount() > 0) {
-  //           $data = $sql->fetchAll(PDO::FETCH_ASSOC);
-  //       } else {
-  //           $data = array();
-  //       }
-  //       return $data;
-  //   } catch (PDOException $e) {
-  //       echo "Erro: " . $e->getMessage();
-  //   }
-  // }
+  }    
 }
