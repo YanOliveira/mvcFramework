030c42c59eac00f8a4a5852af6733926006f858c
Yan Oliveira <oliveira.yan02@gmail.com>
2019-09-04 18:20:37 -0300

    composer e monolog

--- mvc/index.php
+++ mvc/index.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
+require "vendor/autoload.php";
 require "config.php";
 require "autoload.php";
 require 'routes.php';
+
+$log = new Monolog\Logger("log");
+$log->pushHandler(new Monolog\Handler\StreamHandler("errors.log", Monolog\Logger::WARNING));
+// $log->addError("acesso ao sistema.");
 
 $app = new app;
 $app->run();
