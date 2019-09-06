93cc9258dbbe286d803f6f961298ae77d3976a9e
Yan Oliveira <oliveira.yan02@gmail.com>
2019-09-05 22:10:55 -0300

    estruturaçao diretorio de Logs

--- mvc/Helpers/Log.php
+++ mvc/Helpers/Log.php
@@ -8,8 +8,10 @@ class Log
 {
   public function __construct($type, $error)
   {
-    $log = new Logger($type);
-    $log->pushHandler(new StreamHandler("errors.log", Logger::WARNING));
+    $log = explode("_", $type, 2);
+    $filename = strtolower($log[0]);
+    $log = new Logger(strtoupper($log[1]));
+    $log->pushHandler(new StreamHandler("Logs/" . $filename . ".log", Logger::WARNING));
     $log->addError($error);
   }
 }
