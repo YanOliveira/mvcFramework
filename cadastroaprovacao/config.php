323dcc5cf61bb61e40136ea0a0d646b14d41ec7b
Yan Oliveira <oliveira.yan02@gmail.com>
2019-09-06 18:22:01 -0300

    remoção de arquivos desnecessários

--- cadastroaprovacao/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  try{
-    define("DSN", "mysql:dbname=cadastro_autorizacao;host=localhost");
-    define("DBUSER", "root");
-    define("DBPASS", "root");
-    $pdo = new PDO(DSN, DBUSER, DBPASS);    
-  }catch(PDOException $e){
-    echo "Erro: ".$e->getMessage();
-  }
