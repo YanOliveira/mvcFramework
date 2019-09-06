d03245c735168550f0091c7b83214cb5588e81da
Yan Oliveira <oliveira.yan02@gmail.com>
2019-09-06 18:25:53 -0300

    alteraçao base_url

--- src/Config/constants.php
+++ src/Config/constants.php
@@ -5,7 +5,7 @@ define("DB_NAME", "mvc");
 define("DB_USER", "root");
 define("DB_PASS", "root");
 define('CHARSET', 'utf8');
-define("BASE_URL", "http://localhost/projects/b7/mvc/public");
+define("BASE_URL", "http://localhost/mvcFramework/public");
 define("BASE_DIR", __DIR__ . "/../..");
 
 define("DEFAULT_CONTROLLER", "index");
